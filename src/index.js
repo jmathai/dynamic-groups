e6811dc4e871e9db240f4034b1613656e210d0fb
Jaisen Mathai <jaisen@jmathai.com>
2018-03-22 23:55:53 -0700

    initial change

--- src/index.js
+++ src/index.js
@@ -10,18 +10,175 @@
  *    event.{pathParam}, Path parameters as defined in your .joule.yml
  *    event.{queryStringParam}, Query string parameters as defined in your .joule.yml
  */
-var Response = require('joule-node-response');
+var Response = require('joule-node-response')
+    , JouleNodeDatabase = require('joule-node-database')
+    , myDb = new JouleNodeDatabase()
+    , {google} = require('googleapis')
+    , jexl = require('jexl')
+    , scopes = [
+      'https://www.googleapis.com/auth/admin.directory.user'
+      , 'https://www.googleapis.com/auth/admin.directory.group'
+    ];
 
-exports.handler = function(event, context) {
-	var response = new Response();
+const authClient = new google.auth.JWT(
+        process.env.CLIENT_EMAIL,
+        null,
+        process.env.PRIVATE_KEY,
+        scopes,
+        process.env.IMPERSONATE_EMAIL
+      )
+      , admin = google.admin('directory_v1');
+
+jexl.addTransform('lower', function(val) {
+    return val.toLowerCase();
+});
+
+var handler = function(event, context) {
+	var response = new Response()
+      , pathArray = event.path
+      , httpMethod = event.httpMethod;
 	response.setContext(context);
 
-  var name = event.query['name'] || 'World';
-  var greeting = 'Hello, ' + name + '.';
-
-  var result = {
-    "message": greeting
-  };
-  
-  response.send(result);
+  if(pathArray.length === 0) {
+    // base path is the webhook
+    webhook(event, context, response);
+  } else {
+    switch(pathArray[0]) {
+      case 'api':
+        switch(httpMethod) {
+          case 'GET':
+            api_get(event, context, response);
+            break;
+          case 'POST':
+            api_post(event, context, response);
+            break;
+        }
+        break;
+    }
+  }
 };
+
+var api_get = function(event, context, response) {
+  myDb.get('dynamic-groups').done(function(err, data) {
+    if(err) {
+      console.log("Error fetching database in api_get");
+      console.log(err);
+      response.send(err);
+      return;
+    }
+    response.send(data);
+  });
+};
+
+var api_post = function(event, context, response) {
+  var groupKey = event.post.groupKey
+      , rule = event.post.rule
+      , emptyRules = {};
+  myDb.get('dynamic-groups').done(function(err, records) {
+    if(err || !records) {
+      console.log("Error fetching database in api_post");
+      console.log(err);
+      response.send(err);
+      records = emptyRules;
+    }
+    records[groupKey] = {expression: rule};
+    myDb.set('dynamic-groups', records).done(function(err, data) {
+      if(err) {
+        console.log("Error updating database in api_post");
+        console.log(err);
+        response.send(err);
+        return;
+      }
+      response.send(data);
+    });
+  });
+};
+
+var webhook = function(event, context, response) {
+  const userKey = event.post['id'];
+  memberParams = {userKey:userKey, projection: 'full', auth: authClient};
+  admin.users.get(memberParams, function(err, data) {
+    if (err) {
+      console.log(err);
+      response.send(err.response.data);
+      return;
+    }
+    const user = data.data;
+    /*var rules = {
+      'users-in-pm-department@shelterplus.in': {expression: '"product management" in customSchemas.OrgDetails.Department|lower'}
+      , 'users-in-pm-ou@shelterplus.in': {expression: '"/Product Management" == orgUnitPath'}
+    };*/
+
+    myDb.get('dynamic-groups').done(function(err, groupRules) {
+      if(err) {
+        console.log("Error fetching database in api_get");
+        console.log(err);
+        response.send(err);
+        return;
+      }
+      ruleCount = 0;
+      ruleTotal = Object.keys(rules).length;
+      for(group in groupRules) {
+        process_rule(group, rules[group].expression, user, response);
+      }
+    });
+  });
+};
+
+var process_rule = function(group, rule, user, response) {
+  ruleCount++;
+  authClient.authorize(function(err, data) {
+    if (err) {
+			console.log(err);
+      response.send(err.response.data);
+      return;
+    }
+    
+    admin.users.get(memberParams, function(err, data) {
+      if (err) {
+				console.log(err);
+        response.send(err.response.data);
+        return;
+      }
+
+      const user = data.data;
+
+      console.log(rule);
+      console.log(user);
+      jexl.eval(rule, user, function(err, condition_status) {
+        if(condition_status) {
+          const resource = Object.assign(user, {role: 'MEMBER'});
+          const insertMemberParams = {groupKey: group, resource: resource, auth: authClient};
+          admin.members.insert(insertMemberParams, function(err, data) {
+            if (err) {
+              console.log('add_to_group err');
+              //console.log(err);
+              return;
+            }
+            console.log('add_to_group success');
+            if(ruleCount === ruleTotal) {
+              response.send('done'/*{"user": data.data}*/);
+            }
+            return;
+          });
+        } else {
+          const deleteMemberParams = {groupKey: group, memberKey: user.id, auth: authClient};
+          admin.members.delete(deleteMemberParams, function(err, data) {
+            if (err) {
+              console.log('remove_from_group err');
+              console.log(err);
+              return;
+            }
+            console.log('remove_from_group success');
+            if(ruleCount === ruleTotal) {
+              response.send('done'/*{"user": data.data}*/);
+            }
+            return;
+          });
+        }
+      });
+    });
+  });
+};
+
+exports.handler = handler;
